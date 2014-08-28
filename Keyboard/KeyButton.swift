6f614c9bee5e74a1340352ed0e5902705ed2ca17
Jim Stewart <jim@stewartstuff.com>
2014-08-28 17:55:44 -0500

    Fixed some override runtime errors, still getting issues on loading

--- Keyboard/KeyButton.swift
+++ Keyboard/KeyButton.swift
@@ -28,6 +28,6 @@ class KeyButton: UIButton {
     }
 
     required init(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: aDecoder)
     }
 }
--- Keyboard/KeyboardViewController.swift
+++ Keyboard/KeyboardViewController.swift
@@ -35,15 +35,6 @@ class KeyboardViewController: UIInputViewController {
     var spacePressed = false
     var spaceTimer: NSTimer?
     
-
-    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
-        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
-    }
-    
-    required init(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-
     override func updateViewConstraints() {
         super.updateViewConstraints()
     }
